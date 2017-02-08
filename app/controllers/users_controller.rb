2f1b3f5b95abc40160f87eb35dd0ab2d7b54de06
akalabic14 <akalabic14@raf.edu.rs>
2017-02-08 00:33:01 +0000

    Made users controller and users signup page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+
+# def index
+#   @users = User.all
+# end
+  
+  def new
+    @user = User.new
+  end
+  
+# def edit
+#   @user = User.find(params[:id])
+# end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the alpha-blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+  
+# def show
+#   @user = User.find(params[:id])
+# end
+  
+# def destroy
+#   @user = User.find(params[:id])
+#   @user.destroy
+#   flash[:danger] = "The user was successfully deleted"
+#   redirect_to articles_path
+# end
+  
+# def update
+#   @user = User.find(params[:id])
+#   if @user.update(user_params)
+#     flash[:success] = "User was successfully updated"
+#     redirect_to user_path(@user)
+#   else
+#     render 'edit'
+#   end
+# end
+  
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+  
+end
