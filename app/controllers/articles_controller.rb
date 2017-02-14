aef4c96b351e1b64abfcd45a53113ab200dd9b6a
akalabic14 <akalabic14@raf.edu.rs>
2017-02-14 14:12:07 +0000

    added admin functionality and desc order for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
   before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 5)
+    @articles = Article.reorder('updated_at DESC').paginate(page: params[:page], per_page: 5)
   end
   
   def new
@@ -55,7 +55,7 @@ class ArticlesController < ApplicationController
   end
   
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user and !current_user.admin?
       flash[:danger] = "You can only edit or delete your own articles"
       redirect_to root_path
     end
