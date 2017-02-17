7ec47595adbd68ced8633e2ed29ef81fae37f898
akalabic14 <akalabic14@raf.edu.rs>
2017-02-17 00:21:20 +0000

    made article_category table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214172110) do
+ActiveRecord::Schema.define(version: 20170217001057) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
